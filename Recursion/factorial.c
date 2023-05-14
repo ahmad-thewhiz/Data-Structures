e3bab775ae0ff78bbcd8ec0af630d932ed902f54
Ahmad Raza <114557897+ahmad-thewhiz@users.noreply.github.com>
2023-05-14 21:26:03 +0530

    Create factorial.c

---
+++ Recursion/factorial.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int fact(int num) {
+    if(num==1)
+    return 1;
+    
+    return num*fact(num-1);
+}
+
+int main() {
+    int num;
+    scanf("%d",&num);
+    printf("%d",fact(num));
+}
