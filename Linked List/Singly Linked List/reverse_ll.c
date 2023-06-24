62c63bba68d74ef38d8c2f0115906130abcc7322
Ahmad Raza <114557897+ahmad-thewhiz@users.noreply.github.com>
2023-06-24 22:56:19 +0530

    Add files via upload

---
+++ Linked List/Singly Linked List/reverse_ll.c
@@ -0,0 +1,67 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+typedef struct node {
+    int data;
+    struct node *next;
+}node;
+
+node* insert_node(node* head, int data) {
+    node* newNode = (node*)malloc(sizeof(node));
+    newNode->data = data;
+    newNode->next = NULL;
+
+    if (head == NULL) 
+        head = newNode;
+    else {
+        node* curr = head;
+        while (curr->next != NULL) 
+            curr = curr->next;
+        curr->next = newNode;
+    }
+    return head;
+}
+
+node* reverse_ll(node* head) {
+    node* prev=NULL;
+    node* curr=head;
+    node* next=NULL;
+
+    if(head==NULL)
+        return NULL;
+
+    while(curr!=NULL) {
+        next=curr->next;
+        curr->next=prev;
+        prev=curr;
+        curr=next;
+    }
+    head = prev;
+    return head;
+}
+
+void display(node* head) {
+    while(head!=NULL) {
+        printf("%d",head->data);
+        if(head->next!=NULL)
+            printf("->");
+        head=head->next;
+    }
+}
+
+int main() {
+    node* head=NULL;
+    int size;
+    scanf("%d",&size);
+    for(int i=0;i<size;i++) {
+        int val;
+        scanf("%d",&val);
+        head=insert_node(head, val);
+    }
+    printf("Before Reverse: ");
+    display(head);
+    head=reverse_ll(head);
+    printf("\nAfter Reverse: ");
+    display(head);
+    return 0;
+}
