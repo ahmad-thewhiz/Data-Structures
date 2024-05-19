1aa89c98dc2dc2b2db5e6e712b9b06d7cd810c4a
Ahmad Raza <114557897+ahmad-thewhiz@users.noreply.github.com>
2024-05-20 01:39:17 +0530

    Update rod_cutting_brute_force.cpp

--- Dynamic Programming/rod_cutting_brute_force.cpp
+++ Dynamic Programming/rod_cutting_brute_force.cpp
@@ -4,29 +4,28 @@
 
 using namespace std;
 
-
 int rodCutting(const vector<int>& prices, int n) {
-    // Create a DP array to store the maximum revenue obtainable for each length
-    vector<int> dp(n + 1, 0);
+    vector<vector<int>> dp(prices.size() + 1, vector<int>(n + 1, 0));
 
-    // Fill the DP array
-    for (int i = 1; i <= n; i++) {
-        int max_val = 0;
-        for (int j = 0; j < i; j++) {
-            max_val = max(max_val, prices[j] + dp[i - j - 1]);
+    for (int i = 1; i <= prices.size(); i++) {
+        for (int j = 1; j <= n; j++) {
+            if (i <= j) {
+                dp[i][j] = max(dp[i-1][j], prices[i-1] + dp[i][j-i]);
+            } else {
+                dp[i][j] = dp[i-1][j];
+            }
         }
-        dp[i] = max_val;
     }
-      
-    // The maximum revenue obtainable for length n is stored in dp[n]
-    return dp[n];
+    
+    // The maximum revenue obtainable for length n is stored in dp[prices.size()][n]
+    return dp[prices.size()][n];
 }
 
 int main() {
-    vector<int> prices = {2,5,7,8};
+    vector<int> prices = {2, 5, 7, 13};
     
     int n;
-    cin>>n;
+    cin >> n;
 
     int maxRevenue = rodCutting(prices, n);
     cout << "Maximum revenue obtainable: " << maxRevenue << endl;
