10fbbdb0e718933add1746e644c3315f85ec8e80
Ahmad Raza <114557897+ahmad-thewhiz@users.noreply.github.com>
2024-12-25 15:10:19 +0530

    Update delete_from_heap.c

--- Heap/delete_from_heap.c
+++ Heap/delete_from_heap.c
@@ -1,74 +1,84 @@
 #include<stdio.h>
 
 void insert_in_heap(int arr[], int n) {
-    int temp, i=n;
+    int temp, i = n;
     temp = arr[i];
 
-    //runs until the child node is greater than its parent node
-    while(i>1 && temp>arr[i/2]) {
-        arr[i]=arr[i/2];
-        i=i/2;
+    // Runs until the child node is greater than its parent node
+    while (i > 1 && temp > arr[i / 2]) {
+        arr[i] = arr[i / 2];
+        i = i / 2;
     }
 
-    arr[i]=temp;
+    arr[i] = temp;
 }
 
-int delete_from_heap(int arr[], int n) {
-    int x,i,j,temp;
-
-    int val=arr[1];
-    x=arr[n]; //last element in x
-    arr[1]=arr[n];
-    arr[n] = val; //last element copied to first place because in heap we cannot delete any other element
-    i=1; //pointing to first element
-    j=i*2; //pointing to the child node of i
-
-    while(j<n-1) {
-        if(arr[j+1]>arr[j]) //first check if right child is greater than left child
-            j=j+1; //j now pointing to right child
-        
-        if(arr[i]<arr[j]) { //check is parent node is less than its child node
-            temp = arr[i];
-            arr[i]=arr[j];
-            arr[j]=temp;
-            i=j; //i should point to its child which is greater than it
-            j=j*2; //j should point on its child now
-        }      
-        else 
-            break;
+int delete_from_heap(int arr[], int *n) {
+    if (*n < 1) {
+        printf("Heap is empty!\n");
+        return -1;
     }
+
+    int val = arr[1]; // Root value to be deleted
+    int last = arr[*n]; // Last element in the heap
+    arr[1] = last; // Replace root with the last element
+    (*n)--; // Decrease heap size
+
+    int i = 1, j = 2 * i; // Start heapify from root
+
+    while (j <= *n) {
+        // Check if right child exists and is greater than left child
+        if (j + 1 <= *n && arr[j + 1] > arr[j]) 
+            j = j + 1;
+
+        // If parent is smaller than the larger child
+        if (arr[i] < arr[j]) {
+            int temp = arr[i];
+            arr[i] = arr[j];
+            arr[j] = temp;
+
+            i = j; // Move to the child node
+            j = 2 * i;
+        } else {
+            break;
+        }
+    }
+
     return val;
 }
 
 int main() {
     int n;
     printf("Enter number of nodes: ");
-    scanf("%d",&n);
+    scanf("%d", &n);
 
-    //We have to take first element as 0 because we have to perform insert operation starting from index to n
-    int a[n+1];
-    a[0]=0;
+    // Initialize the array for heap, with size n+1
+    int a[n + 1];
+    a[0] = 0; // Dummy element
 
-    //Taking values from the user
-    for(int i=1;i<=n;i++) 
-        scanf("%d",&a[i]);
+    printf("Enter the elements: ");
+    // Taking values from the user
+    for (int i = 1; i <= n; i++) 
+        scanf("%d", &a[i]);
 
-    //Calling the function to perform insertion in heap
-    for(int i=2;i<=n;i++)
-        insert_in_heap(a,i);
+    // Calling the function to perform insertion in heap
+    for (int i = 2; i <= n; i++)
+        insert_in_heap(a, i);
 
-    //Display
-    for(int i=1;i<=n;i++)
+    printf("Heap after insertion: ");
+    for (int i = 1; i <= n; i++)
         printf("%d ", a[i]);
+    printf("\n");
 
-    int index;
-    printf("\nEnter index of the element to be deleted: ");
-    scanf("%d",&index);
-    printf("\nDeleted element from the heap: %d\n",delete_from_heap(a,index));
+    // Deleting the root element from the heap
+    int deleted_val = delete_from_heap(a, &n);
+    if (deleted_val != -1) 
+        printf("Deleted element from the heap: %d\n", deleted_val);
 
-    //Display
-    for(int i=1;i<=n;i++)
+    printf("Heap after deletion: ");
+    for (int i = 1; i <= n; i++)
         printf("%d ", a[i]);
+    printf("\n");
 
     return 0;
 }
